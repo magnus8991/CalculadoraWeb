1162df74e8ccade9d8a4aadf7575cc1a5fbf9734
magnus8991 <dalfredomandon@unicesar.edu.co>
2020-09-21 13:02:34 -0500

    terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { DivisionComponent } from './division/division.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { MultiplicacionComponent } from './multiplicacion/multiplicacion.component';
 import { RestaComponent } from './resta/resta.component';
 import { SumaComponent } from './suma/suma.component';
 
-const routes: Routes = [
-  {path: 'inicio', component: InicioComponent},
-  {path: 'suma', component: SumaComponent},
-  {path: 'resta', component: RestaComponent},
-  {path: 'multiplicacion', component: MultiplicacionComponent},
-  {path: 'division', component: DivisionComponent}
-];
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'inicio', component: InicioComponent },
+    { path: 'suma', component: SumaComponent },
+    { path: 'resta', component: RestaComponent },
+    { path: 'multiplicacion', component: MultiplicacionComponent },
+    { path: 'division', component: DivisionComponent },
+    { path: '', component: InicioComponent }
+  ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
