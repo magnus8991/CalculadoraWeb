aea69d3990111a9ed2694feb372b8641e12722b7
magnus8991 <dalfredomandon@unicesar.edu.co>
2020-09-24 10:08:37 -0500

    weje :V

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { DivisionComponent } from './division/division.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { MultiplicacionComponent } from './multiplicacion/multiplicacion.component';
 import { RestaComponent } from './resta/resta.component';
 import { SumaComponent } from './suma/suma.component';
 
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'suma', component: SumaComponent },
+  { path: 'resta', component: RestaComponent },
+  { path: 'multiplicacion', component: MultiplicacionComponent },
+  { path: 'division', component: DivisionComponent }
+]
+
 @NgModule({
-  imports: [RouterModule.forRoot([
-    { path: 'inicio', component: InicioComponent },
-    { path: 'suma', component: SumaComponent },
-    { path: 'resta', component: RestaComponent },
-    { path: 'multiplicacion', component: MultiplicacionComponent },
-    { path: 'division', component: DivisionComponent },
-    { path: '', component: InicioComponent }
-  ])
+  imports: [RouterModule.forRoot(routes),
+    RouterModule.forRoot([{ path: '', component: InicioComponent }])
   ],
   exports: [RouterModule]
 })
